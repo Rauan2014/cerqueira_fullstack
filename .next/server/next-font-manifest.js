647b28005ddce9cd42aef16f477b23fa732e62d5
carnivorazilla <rauanbernardo@gmail.com>
2025-07-26 14:17:19 -0300

    Clear cache and use Cloudflare function only

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/mnt/c/Users/win/Desktop/arquivos_secretos/cerqueira-fullstack/src/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
